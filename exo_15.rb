fd37ac340a7c3c99d2567d36ed11c856cb9dbc01
LoulergueC <clement.loulergue@live.fr>
2023-10-03 12:15:41 +0200

    exo 15

---
+++ exo_15.rb
@@ -0,0 +1,8 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> "
+n = gets.chomp.to_i
+
+puts "Voici la pyramide :"
+(n).times do |i|
+    puts "#" * (i + 1)
+end
